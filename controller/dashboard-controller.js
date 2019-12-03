05081c2523527a378bd38bd29c7b7ccab6b8b8a1
Joannerpena <joanner.c1006@gmail.com>
2019-12-03 12:55:03 -0500

    Refactoring some code from Dashboard and Commenting

--- controller/dashboard-controller.js
+++ controller/dashboard-controller.js
@@ -1,3 +1,4 @@
+// DOM Interactions
 var bankAmount = document.querySelector('#amount');
 var bankSpent = document.querySelector('#spent');
 var profileName = document.querySelector('#userName');
@@ -8,23 +9,44 @@ var tableItem = document
   .querySelector('#transaction-item')
   .getElementsByTagName('tbody')[0];
 
+// Form Transfer
 var accountID = document.forms[0].accountID;
 var transferAmount = document.forms[0].transferAmount;
 var submitTransferButton = document.querySelector('#submitTransferButton');
 
+// Form New Transaction
 var inputAmount = document.forms[1].InputAmount;
 var inputDesc = document.forms[1].InputDesc;
 var selectClass = document.forms[1].selectClass;
 var submitButton = document.querySelector('#submitButton');
 
+// Set date for transactions
 var today = new Date();
 var date =
-  today.getMonth() + 1 + '-' + today.getDate() + '-' + today.getFullYear();
+  today.getMonth() + 1 + '-' + today.getDate() + '-' + today.getFullYear(); // 12-3-2019
 
+// Retrieve from LocalStorage
 var logIndex = localStorage.getItem('index');
 var DumpUsers = JSON.parse(localStorage.getItem('DumpUsers'));
 var transactionItem = JSON.parse(localStorage.getItem('transactionList'));
 
+// Use the Main function to work with onLoad callings.
+function main() {
+  document.forms[0].reset();
+
+  fillDashboard();
+  fillTransactionList();
+
+  logOutLink.addEventListener('click', logOut);
+  submitButton.addEventListener('click', createTransaction);
+  submitTransferButton.addEventListener('click', createTransfer);
+
+  profileName.addEventListener('click', function() {
+    window.location.replace('../pages/dashboard.html');
+  });
+}
+
+// When log out remove Index and isLog from LocalStorage.
 function logOut() {
   localStorage.removeItem('index');
   localStorage.removeItem('isLog');
@@ -32,17 +54,17 @@ function logOut() {
   window.location.replace('../index.html');
 }
 
+// Fill the dashboard with the user's information.
 function fillDashboard() {
-  for (var i = 0; i < DumpUsers.length; i++) {
-    if (DumpUsers[i][0] == logIndex) {
-      profileName.innerText = DumpUsers[i][1] + ' ' + DumpUsers[i][2];
-      profileImage.style.backgroundImage = 'url(' + DumpUsers[i][7] + ')';
-      bankAmount.innerText = '$' + DumpUsers[i][5];
-      bankSpent.innerText = '$' + DumpUsers[i][6];
-    }
-  }
+  var accountLogged = findAccount(logIndex);
+
+  profileName.innerText = accountLogged[1] + ' ' + accountLogged[2];
+  profileImage.style.backgroundImage = 'url(' + accountLogged[7] + ')';
+  bankAmount.innerText = '$' + accountLogged[5];
+  bankSpent.innerText = '$' + accountLogged[6];
 }
 
+// Fill the transactions from this user to the dashboard.
 function fillTransactionList() {
   var myTransaction = new Array();
 
@@ -54,6 +76,8 @@ function fillTransactionList() {
 
   myTransaction.reverse();
 
+  // When the information have been fetched, we start to fill
+  // the table with the multiple information.
   for (var i = 0; i < myTransaction.length; i++) {
     var tableRow = tableItem.insertRow(i);
     var transactionID = tableRow.insertCell(0);
@@ -68,6 +92,8 @@ function fillTransactionList() {
   }
 }
 
+// Take the data from the Form New Transaction
+// and create a new transaction.
 function createTransaction() {
   var Index = logIndex - 1;
 
@@ -94,13 +120,15 @@ function createTransaction() {
   window.location.reload();
 }
 
+// Sent money from user to another account.
 function createTransfer() {
   var transaction = transferMoney();
-  transferTransaction(transaction[0], transaction[1]);
+  transferTransaction(transaction[0], transaction[1]); // Transaction, Account
 
   window.location.reload();
 }
 
+// Create transfer transaction to be sent and to be receive.
 function transferTransaction(transaction, account) {
   var Index = logIndex - 1;
   var thisUser = findAccount(logIndex);
@@ -123,10 +151,10 @@ function transferTransaction(transaction, account) {
     date
   ];
 
-  DumpUsers[Index][5] = DumpUsers[Index][5] - parseInt(transferAmount.value);
-  DumpUsers[Index][6] = DumpUsers[Index][6] + parseInt(transferAmount.value);
+  DumpUsers[Index][5] = DumpUsers[Index][5] - parseInt(transferAmount.value); // Balance - Transfer money
+  DumpUsers[Index][6] = DumpUsers[Index][6] + parseInt(transferAmount.value); // Spent + Transfer money
   DumpUsers[account[0] - 1][5] =
-    DumpUsers[account[0] - 1][5] + parseInt(transferAmount.value);
+    DumpUsers[account[0] - 1][5] + parseInt(transferAmount.value); //Another User Balance + Transfer money
 
   transactionItem.push(newTransaction);
   transactionItem.push(transactionTo);
@@ -135,6 +163,7 @@ function transferTransaction(transaction, account) {
   localStorage.setItem('DumpUsers', JSON.stringify(DumpUsers));
 }
 
+//If accountID found, get user information, else alert 'not found'
 function transferMoney() {
   var accountToTransfer = findAccount(accountID.value);
   var transferInfo = null;
@@ -149,6 +178,7 @@ function transferMoney() {
   return transferInfo;
 }
 
+// Given an ID get the user information.
 function findAccount(id) {
   var accountFound = new Array();
 
@@ -159,21 +189,6 @@ function findAccount(id) {
   }
 
   return accountFound;
-}
-
-function main() {
-  document.forms[0].reset();
-
-  fillDashboard();
-  fillTransactionList();
-
-  logOutLink.addEventListener('click', logOut);
-  submitButton.addEventListener('click', createTransaction);
-  submitTransferButton.addEventListener('click', createTransfer);
-
-  profileName.addEventListener('click', function() {
-    window.location.replace('../pages/dashboard.html');
-  });
 }
 
 main();
