a4c2121bc40914005a8abaf873db61ee278bbaf2
Joannerpena <joanner.c1006@gmail.com>
2019-12-05 01:34:45 -0500

    Added validation for missing fields for registration

--- controller/register-controller.js
+++ controller/register-controller.js
@@ -22,6 +22,12 @@ function main() {
   submitButton.addEventListener('click', registerUser);
   closeAlert.addEventListener('click', closeAlertNotificacion);
 
+  inputName.addEventListener('focus', function() {
+    inputName.classList.remove('is-invalid');
+  });
+  inputLName.addEventListener('focus', function() {
+    inputLName.classList.remove('is-invalid');
+  });
   inputEmail.addEventListener('keydown', function() {
     inputEmail.classList.remove('is-invalid');
   });
@@ -31,29 +37,36 @@ function main() {
 }
 
 function registerUser() {
+  var formValidated = formVerification();
   validated = validateUser();
-  if (validated == true) {
-    if ('DumpUsers' in localStorage) {
-      var index = DumpUsers.length + 1;
-      var newUser = [
-        index,
-        inputName.value,
-        inputLName.value,
-        inputEmail.value,
-        inputPass.value,
-        parseInt(inputAmount.value),
-        0,
-        '../img/default-profile.jpg'
-      ];
 
-      DumpUsers.push(newUser);
-      localStorage.setItem('DumpUsers', JSON.stringify(DumpUsers));
+  if (formValidated) {
+    if (validated == true) {
+      if ('DumpUsers' in localStorage) {
+        var index = DumpUsers.length + 1;
+        var newUser = [
+          index,
+          inputName.value,
+          inputLName.value,
+          inputEmail.value.toLowerCase(),
+          inputPass.value,
+          parseInt(inputAmount.value),
+          0,
+          '../img/default-profile.jpg'
+        ];
 
-      localStorage.setItem('isLog', true);
-      localStorage.setItem('index', index);
-      window.location.replace('../pages/dashboard.html');
+        DumpUsers.push(newUser);
+        localStorage.setItem('DumpUsers', JSON.stringify(DumpUsers));
+
+        localStorage.setItem('isLog', true);
+        localStorage.setItem('index', index);
+        window.location.replace('../pages/dashboard.html');
+      }
+    } else {
+      return;
     }
   } else {
+    triggerAlert('Some needed field are missing.');
     return;
   }
 }
@@ -88,6 +101,27 @@ function triggerAlert(text) {
 
 function closeAlertNotificacion() {
   formAlert.classList.remove('show');
+}
+
+function formVerification() {
+  if (inputName.value == '') {
+    inputName.classList.add('is-invalid');
+    return false;
+  } else if (inputLName.value == '') {
+    inputLName.classList.add('is-invalid');
+    return false;
+  } else if (inputEmail.value == '') {
+    inputEmail.classList.add('is-invalid');
+    return false;
+  } else if (inputPass.value == '') {
+    inputPass.classList.add('is-invalid');
+    return false;
+  } else if (inputAmount.value == '') {
+    inputAmount.classList.add('is-invalid');
+    return false;
+  } else {
+    return true;
+  }
 }
 
 main();
